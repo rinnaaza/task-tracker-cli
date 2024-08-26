55c59a22f0a9f6333973b56cf894081bdf744262
rinnaaza <linna.azaiez@gmail.com>
2024-08-26 12:46:03 +0100

    Fix printHelp function

--- src/print-help.ts
+++ src/print-help.ts
@@ -1,13 +1,13 @@
 const printHelp = (): void => {
     console.log(`
-        Usage: mycli <command> [options]
+        Usage: task-cli <command> [options]
         Commands:
         add <task>                          Add new task
         update <index> <task>               Update existing task at a specific index.
         delete <index>                      Delete existing task at a specific index.
-        update mark-in-progress <index>     Update status of existing task at a specific index to in progress.
-        update mark-done <index>            Update status of existing task at a specific index to done.
-        update mark-not-done <index>        Update status of existing task at a specific index to not done.
+        mark-in-progress <index>            Update status of existing task at a specific index to in progress.
+        mark-done <index>                   Update status of existing task at a specific index to done.
+        mark-not-done <index>               Update status of existing task at a specific index to not done.
         list                                List all tasks.
         list in-progress                    List in progress tasks.
         list done                           List all done tasks.
