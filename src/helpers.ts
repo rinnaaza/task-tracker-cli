3f03056cb17793661eee25d4c529a91c35aeb8f1
rinnaaza <linna.azaiez@gmail.com>
2024-09-03 22:42:20 +0100

    Remove helper function

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,9 @@
 import { v4 as uuidv4 } from "uuid";
 
-const generateId = () :string => {
-    return uuidv4();
-}
+const generateId = () :string => uuidv4();
 
 const inRange = (index: number, min: number, max: number) : boolean => (
     (index >= min) && (index <= max)
 );
 
-const notInRange = (index: number, min: number, max: number) : boolean => !(inRange(index, min, max));
-
-export { generateId, inRange, notInRange };
+export { generateId, inRange };
